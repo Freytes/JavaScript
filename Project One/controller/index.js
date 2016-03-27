f35da08f4fa5f8aaa92689818a95f69c80936872
Cfreytes <Cfreytez@gmail.com>
2016-03-27 13:44:45 -0400

    JavaScript - Project 1

---
+++ Project One/controller/index.js
@@ -0,0 +1,13 @@
+
+//Run by pressing the button
+function runIt()
+{
+	var string="Hello World!";
+	var stringLength=string.length;
+	
+	// Okay rest is up to you! Make the variable stringLength reflect the length of the string above!
+	
+	
+	alert(stringLength);
+	
+}
